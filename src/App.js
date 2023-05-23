74c62c86db77481e732971aac9c5b6a9074a84a7
soroushkhosravi <soroushgol2022@gmail.com>
2023-05-23 16:19:00 +0100

    we can show things based on env variable.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import './App.css';
 
 function App() {
-      if (process.env.NODE_ENV === "development"){
-        return (
-          <div className="App">
-            <p>We are in dev environment.</p>
-            <h1> This is a header. </h1>
-            <p>This is an env variable: {process.env.REACT_APP_ENV_VARIABLE}</p>
-            <p> This is {process.env.NODE_ENV} env variable: {process.env.REACT_APP_CREATURE}</p>
-            <p>This is an env variable: {process.env.REACT_APP_SECOND_ENV_VARIABLE}</p>
-          </div>
-        );
-      };
-      if (process.env.NODE_ENV === "staging"){
-        return (
-          <div className="App">
-            <p>We are in dev environment.</p>
-            <h1> This is a header. </h1>
-            <p>This is an env variable: {process.env.REACT_APP_ENV_VARIABLE}</p>
-            <p> This is {process.env.NODE_ENV} env variable: {process.env.REACT_APP_CREATURE}</p>
-            <p>This is an env variable: {process.env.REACT_APP_SECOND_ENV_VARIABLE}</p>
-          </div>
-        );
-      };
       return (
         <div>
-        <h1> We are in prod or test environment.</h1>
-        <p> This is prod env variable: {process.env.REACT_APP_CREATURE}</p>
+	        <h1> We are in {process.env.REACT_APP_ENV}.</h1>
+	        <p> This is prod env variable animal: {process.env.REACT_APP_CREATURE}</p>
         </div>
       )
 }
