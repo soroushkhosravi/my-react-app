6f93db83b3276381c0fa7fc91d5e3d8201610418
soroushkhosravi <soroushgol2022@gmail.com>
2023-05-23 16:09:53 +0100

    we can set env variables.

--- src/App.js
+++ src/App.js
@@ -7,12 +7,27 @@ function App() {
             <p>We are in dev environment.</p>
             <h1> This is a header. </h1>
             <p>This is an env variable: {process.env.REACT_APP_ENV_VARIABLE}</p>
+            <p> This is {process.env.NODE_ENV} env variable: {process.env.REACT_APP_CREATURE}</p>
+            <p>This is an env variable: {process.env.REACT_APP_SECOND_ENV_VARIABLE}</p>
+          </div>
+        );
+      };
+      if (process.env.NODE_ENV === "staging"){
+        return (
+          <div className="App">
+            <p>We are in dev environment.</p>
+            <h1> This is a header. </h1>
+            <p>This is an env variable: {process.env.REACT_APP_ENV_VARIABLE}</p>
+            <p> This is {process.env.NODE_ENV} env variable: {process.env.REACT_APP_CREATURE}</p>
             <p>This is an env variable: {process.env.REACT_APP_SECOND_ENV_VARIABLE}</p>
           </div>
         );
       };
       return (
+        <div>
         <h1> We are in prod or test environment.</h1>
+        <p> This is prod env variable: {process.env.REACT_APP_CREATURE}</p>
+        </div>
       )
 }
 
