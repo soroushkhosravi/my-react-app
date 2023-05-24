273b8b96f9d574cb79b908c0012b28d8a7c66db6
soroushkhosravi <soroushgol2022@gmail.com>
2023-05-24 11:58:15 +0100

    command will be running for docker compose.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
       return (
         <div>
 	        <h1> We are in {process.env.REACT_APP_ENV}.</h1>
-	        <p>Hello should be deleted.</p>
+	        <p>Command in docker compose file not yet.</p>
 	        <p> This is prod env variable animal: {process.env.REACT_APP_CREATURE}</p>
         </div>
       )
