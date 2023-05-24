21b2dce01ca607f9a41cd22849c4c7e47606d4e4
soroushkhosravi <soroushgol2022@gmail.com>
2023-05-24 11:49:56 +0100

    the app is dockerised now.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
       return (
         <div>
 	        <h1> We are in {process.env.REACT_APP_ENV}.</h1>
+	        <p>Hello should be deleted.</p>
 	        <p> This is prod env variable animal: {process.env.REACT_APP_CREATURE}</p>
         </div>
       )
