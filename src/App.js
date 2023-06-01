a6247ea1fb80cdbd00e6e3dd359481ad3f0cda08
soroushkhosravi <soroushgol2022@gmail.com>
2023-06-01 09:40:58 +0100

    The login button is added.

--- src/App.js
+++ src/App.js
@@ -14,15 +14,16 @@ function createToken(token){
 }
 
 function SetInitials(){
-	const [renderComponent, setRenderComponent] = useState(false);
+	const [userLoggedIn, setUserLoggedIn] = useState(false);
 	const [username, setUserName] = useState(null);
 	const [email, setEmail] = useState(null);
 	const [jwt_token] = useState(localStorage.getItem("jwt_token"))
 	const [apiRead, setApiRead] = useState(false);
 	const [apiError, setApiError] = useState(null);
+	const [authURL, setAuthURL] = useState(null);
 	return [
-		renderComponent,
-		setRenderComponent,
+		userLoggedIn,
+		setUserLoggedIn,
 		username,
 		setUserName,
 		email,
@@ -31,15 +32,17 @@ function SetInitials(){
 		apiRead,
 		setApiRead,
 		apiError,
-		setApiError
+		setApiError,
+		authURL,
+		setAuthURL,
 	];
 };
 
 
 function App(props) {
 	const [
-		renderComponent,
-		setRenderComponent,
+		userLoggedIn,
+		setUserLoggedIn,
 		username,
 		setUserName,
 		email,
@@ -48,7 +51,9 @@ function App(props) {
 		apiRead,
 		setApiRead,
 		apiError,
-		setApiError
+		setApiError,
+		authURL,
+		setAuthURL,
 	] = SetInitials();
 
 	useEffect(() => {
@@ -63,24 +68,34 @@ function App(props) {
 				)
 				const json_response = await response.json();
 				if(json_response.message === 'user not logged in.'){
-					window.location.assign(json_response.auth_url + '?next_url=' + signInCallbackURL());
+//					window.location.assign(json_response.auth_url + '?next_url=' + signInCallbackURL());
+					setUserLoggedIn(false);
+					setAuthURL(json_response.auth_url);
+					setApiRead(true);
 				}
 				if(json_response.message === 'current user is found.'){
 					const user_data = JSON.parse(json_response.user)
 					setUserName(user_data.username);
 					setEmail(user_data.email);
-					setRenderComponent(true);
+					setUserLoggedIn(true);
 					setApiRead(true);
 				}
 			} catch (error){
-				setRenderComponent(true);
+				setUserLoggedIn(true);
 				setApiRead(false);
 				setApiError(error.message);
 			}
 		}
 		setToken();
-	}, [jwt_token, setApiError, setApiRead, setEmail, setUserName, setRenderComponent,])
-	if (renderComponent === true && apiRead === true){
+	}, [jwt_token, setApiError, setApiRead, setEmail, setUserName, setUserLoggedIn, setAuthURL])
+	if (userLoggedIn === false && apiRead === true){
+		return(
+			<div>
+			 < LogIn auth_url={authURL} />
+			</div>
+		)
+	}
+	if (userLoggedIn === true && apiRead === true){
 	    return (
 			<div>
 				< LogOut />
@@ -90,7 +105,7 @@ function App(props) {
 			</div>
 		)
 	}
-	if (renderComponent === true && apiRead === false){
+	if (userLoggedIn === true && apiRead === false){
 	    return (
 			<div>
 				< LogOut />
@@ -111,13 +126,16 @@ function SetJWT(){
 function LogOut(){
 	function removeJWT(){
 		localStorage.removeItem("jwt_token");
-		window.location.assign(window.origin)
+		window.location.assign(window.origin);
 	}
-	return (
-	    <button onClick={removeJWT}>
-	      Click me to log out.
-	    </button>
-    );
+	return (<button onClick={removeJWT}>Click me to log out.</button>);
+}
+
+function LogIn(props){
+	function redirectToLogInPage(){
+		window.location.assign(props.auth_url + '?next_url=' + signInCallbackURL());
+	}
+	return (<button onClick={redirectToLogInPage}> Log In </button>);
 }
 
 export {App, SetJWT, LogOut};
