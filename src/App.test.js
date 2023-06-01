09c76d2c1d45a331d67d3ec69e45f79f52f33653
soroushkhosravi <soroushgol2022@gmail.com>
2023-06-01 12:08:37 +0100

    test name is changed.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import {App} from './App';
 
-test('renders learn react link', () => {
+test('App rendering because of backend not responding.', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Application not available because of/i);
+  const linkElement = screen.getByText(/Application not available because of:/i);
   expect(linkElement).toBeInTheDocument();
 });
