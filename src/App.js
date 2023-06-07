179f3d64e7a90a2f3f25a55be62e8cda4b1638b2
soroushkhosravi <soroushgol2022@gmail.com>
2023-06-07 11:10:12 +0100

    The protected route is added.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {useParams, useNavigate} from "react-router-dom";
 import { useState, useEffect} from 'react';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
 function signInCallbackURL(){
 	return window.location.origin + '/jwt';
@@ -43,7 +43,7 @@ function SetInitials(){
 };
 
 
-function App(props) {
+function App() {
 	const [
 		userLoggedIn,
 		setUserLoggedIn,
@@ -122,11 +122,78 @@ function App(props) {
 	}
 }
 
+function Home(){
+	return(
+		<div>Hello this is home man.</div>
+	)
+}
+
+const ProtectedRoute = ({children}) => {
+		const [
+		userLoggedIn,
+		setUserLoggedIn,
+		username,
+		setUserName,
+		email,
+		setEmail,
+		jwt_token,
+		apiRead,
+		setApiRead,
+		apiError,
+		setApiError,
+		authURL,
+		setAuthURL,
+		pageLoaded,
+		setPageLoaded
+	] = SetInitials();
+		useEffect(() => {
+		async function setToken(){
+			try {
+				const response = await fetch(
+					userURL(),
+					{
+						method: 'GET',
+						headers: {'Authentication': createToken(jwt_token)}
+					}
+				)
+				const json_response = await response.json();
+				setApiRead(true);
+				if(json_response.message === 'user not logged in.'){
+					setUserLoggedIn(false);
+					setAuthURL(json_response.auth_url);
+				}
+				if(json_response.message === 'current user is found.'){
+					const user_data = JSON.parse(json_response.user)
+					setUserName(user_data.username);
+					setEmail(user_data.email);
+					setUserLoggedIn(true);
+				}
+				setPageLoaded(true);
+			} catch (error){
+			    setPageLoaded(true);
+				setApiRead(false);
+				setApiError(error.message);
+			}
+		}
+		setToken();
+	}, [jwt_token, setApiError, setApiRead, setEmail, setUserName, setUserLoggedIn, setAuthURL, setPageLoaded])
+	if (apiRead === true & userLoggedIn === true){
+		return children
+	} else if (pageLoaded === true){
+		return <Navigate to='/' />;
+	}
+};
+
 function CompleteApp(){
   return(
     <BrowserRouter>
 		<Routes>
-            <Route path="/" element={< App name="App" names={["soroush", "farnaz"]} users={[10, 12]} />} />
+            <Route path="/" element={< App />} />
+            <Route path="/home" element={
+                <ProtectedRoute>
+                    <Home/>
+                </ProtectedRoute>
+            } ></Route>
 			<Route path="/jwt/:jwt" element={< SetJWT />} />
 			<Route path="/logout" element={< LogOut />} />
         </Routes>
