f26328f86ff10f93ae41cecdbf865c9122b62f41
soroushkhosravi <soroushgol2022@gmail.com>
2023-06-12 16:10:02 +0100

    we can see anti social behaviours.

--- src/Address.js
+++ src/Address.js
@@ -1,14 +1,47 @@
+import { useState, useEffect} from 'react';
+import { createToken } from './helpers'
+
 function Address(){
+	const [postCode, setPostCode] = useState('');
+	const [addressData, setAddressData] = useState({});
+    const handleSubmit = (event) => {
+	    event.preventDefault();
+	    getAddressData();
+    };
+
+	const getAddressData = async function(){
+	const response = await fetch(
+	process.env.REACT_APP_BACKEND_URL + "/api/addresses",
+		{
+			method: 'POST',
+			headers: {
+				'Authentication': createToken(localStorage.getItem("jwt_token")),
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({'post_code': postCode})
+		}
+	)
+	const json_response = await response.json();
+	setAddressData(json_response);
+	}
 	return(
 		<>
 			<h1>Address investigation.</h1>
-			<form >
-				<label>
+			<form onSubmit={handleSubmit} >
 				Post Code:
-				<input type="text"/>
-				</label>
+				<input
+					type="text"
+					id="postCode"
+					name="postCode"
+					value={postCode}
+					placeholder="Post Code"
+					onChange={
+						(event) => {setPostCode(event.target.value);}
+                    }
+				/>
 				<input type="submit" value="Submit" className="btn btn-primary" />
 			</form>
+			<div>{JSON.stringify(addressData.crimes)}</div>
 		</>
 	)
 }
