8726d3e415438a3e779e5856162d434435143489
soroushkhosravi <soroushgol2022@gmail.com>
2023-06-12 12:55:37 +0100

    The class is added to address form.

--- src/App.js
+++ src/App.js
@@ -127,13 +127,13 @@ function Address(){
 	return(
 		<>
 			<h1>Address investigation.</h1>
-		<form >
-			<label>
-			Post Code:
-			<input type="text"/>
-			</label>
-			<input type="submit" value="Submit" />
-		</form>
+			<form >
+				<label>
+				Post Code:
+				<input type="text"/>
+				</label>
+				<input type="submit" value="Submit" className="btn btn-primary" />
+			</form>
 		</>
 	)
 }
