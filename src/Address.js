b45aa253eeb5539225541ada1940a57abd462bef
soroushkhosravi <soroushgol2022@gmail.com>
2023-06-12 17:40:44 +0100

    unauthorised users can not investigate address.

--- src/Address.js
+++ src/Address.js
@@ -1,8 +1,10 @@
 import { useState, useEffect} from 'react';
-import { createToken } from './helpers'
+import { createToken } from './helpers';
+import { BrowserRouter, Routes, Route, Navigate,  Link} from "react-router-dom";;
 
 function Address(){
 	const [postCode, setPostCode] = useState('');
+	const [invalidToken, setInvalidToken] = useState(false);
 	const [addressData, setAddressData] = useState({});
     const handleSubmit = (event) => {
 	    event.preventDefault();
@@ -10,19 +12,26 @@ function Address(){
     };
 
 	const getAddressData = async function(){
-	const response = await fetch(
-	process.env.REACT_APP_BACKEND_URL + "/api/addresses",
-		{
-			method: 'POST',
-			headers: {
-				'Authentication': createToken(localStorage.getItem("jwt_token")),
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({'post_code': postCode})
+		const response = await fetch(
+		process.env.REACT_APP_BACKEND_URL + "/api/addresses",
+			{
+				method: 'POST',
+				headers: {
+					'Authentication': createToken(localStorage.getItem("jwt_token")),
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({'post_code': postCode})
+			}
+		)
+		const json_response = await response.json();
+		if (json_response.message === 'user not logged in.'){
+			setInvalidToken(true);
+			return
 		}
-	)
-	const json_response = await response.json();
-	setAddressData(json_response);
+		setAddressData(json_response);
+	}
+	if (invalidToken === true){
+		return <Navigate to='/' />;
 	}
 	return(
 		<>
@@ -41,7 +50,10 @@ function Address(){
 				/>
 				<input type="submit" value="Submit" className="btn btn-primary" />
 			</form>
-			<div>{JSON.stringify(addressData.crimes)}</div>
+			{"crimes" in addressData && (
+				 Object.entries(addressData.crimes).map((t,k) => <div key={t[0]}> {t[0]}: {t[1]}</div>)
+				 )
+			}
 		</>
 	)
 }
